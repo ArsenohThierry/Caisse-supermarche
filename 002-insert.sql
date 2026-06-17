ca5c47198c5c75c91a3f9a5a907d6ef4492fa019
Hanaa-slay <hanaatdr@gmail.com>
2026-06-17 09:12:37 +0300

    script initialisation base

---
+++ 002-insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO Caisse(designation)
+VALUES
+('Caisse 1'),
+('Caisse 2');
+
+INSERT INTO Produit(label, pu, qteStock)
+VALUES
+('Riz blanc 1kg', 3500, 100),
+('Huile végétale 1L', 12000, 50),
+('Sucre 1kg', 4000, 75),
+('Lait en poudre 500g', 15000, 30),
+('Savon de Marseille', 2500, 120);
